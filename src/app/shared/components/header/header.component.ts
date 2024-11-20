7a6b86c7855c73bb3f2698894899df20dc615fb7
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-11-20 17:46:06 -0600

    modificated: test tournament2

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,11 +7,5 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class HeaderComponent {
 
-  @Input() messageFromParent: string = ''; // Recibe datos del padre
-  @Output() messageToParent: EventEmitter<string> = new EventEmitter(); // Emite eventos al padre
-
-  sendMessage() {
-    this.messageToParent.emit('Mensaje enviado desde el hijo al padre.');
-  }
-
+  
 }
