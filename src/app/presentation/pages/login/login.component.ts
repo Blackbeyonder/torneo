8b1207a6e5efc558b101e5c1705d2486d7d3e4f3
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-11-22 02:33:58 -0600

    modificated: added valid login error

--- src/app/presentation/pages/login/login.component.ts
+++ src/app/presentation/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
@@ -13,6 +13,7 @@ import { Utils } from 'src/app/utils/Utils';
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  displayErrorDialog: boolean = false;
 
   constructor(private fb: FormBuilder, private userService :UserService, private router: Router) {
     // Crear formulario con validadores estándar
@@ -24,14 +25,28 @@ export class LoginComponent {
 
   async onSubmit() {
     if (this.loginForm.valid) {
-      let raw= this.loginForm.getRawValue();
+      let raw = this.loginForm.getRawValue();
       
-     let response: Login = await lastValueFrom(this.userService.getlogin(raw.user,raw.password)); // Convertimos Observable a Promesa y esperamos
-     if(response.success && response.success==true){
-      localStorage.setItem('user', JSON.stringify(response.data)); // Guarda los datos del usuario como JSON
-      localStorage.setItem('token', response.token); // Guarda el token
-      Utils.redirectTo(this.router,"admin");
-     }
+      try {
+        // Convertimos Observable a Promesa y esperamos
+        let response: Login = await lastValueFrom(this.userService.getlogin(raw.user, raw.password));
+        
+        if (response.success && response.success === true) {
+          // Guarda los datos del usuario como JSON
+          localStorage.setItem('user', JSON.stringify(response.data));
+          // Guarda el token
+          localStorage.setItem('token', response.token);
+          // Redirige al panel de admin
+          Utils.redirectTo(this.router, "admin");
+        } else {
+          // Si no es exitoso, muestra el modal de error
+          this.displayErrorDialog = true;
+        }
+      } catch (error) {
+        // Si ocurre un error, muestra el modal de error
+        this.displayErrorDialog = true;
+        console.error('Error al iniciar sesión:', error);
+      }
     } else {
       console.log('Formulario inválido');
     }
